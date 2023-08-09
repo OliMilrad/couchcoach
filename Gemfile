cda84c9088d48387c64e7829cec1e71c6ae80530
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-08-09 15:07:35 +0300

    with evgeny

--- Gemfile
+++ Gemfile
@@ -46,8 +46,9 @@ gem "bootsnap", require: false
 gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-gem "image_processing", "~> 1.2"
-gem "mini_magick"
+# gem "image_processing", "~> 1.2"
+# gem "mini_magick"
+gem "cloudinary"
 
 gem "devise"
 gem "autoprefixer-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
 Coach.destroy_all
 
 coach1 = Coach.new(name: "Евгений", filter: "сила, растяжка, фитбокс")
-photo = File.open('app/assets/images/golub_photo.png')
-coach1.photo.attach(io: photo, filename: "golub_photo")
+photo = URI.open("https://res.cloudinary.com/dzuk6qaao/image/upload/v1691581163/golub_photo_a6hbfe.png")
+coach1.photo.attach(io: photo, filename: "golub_photo", content_type: "image/png")
 coach1.save!
 
-coach2 = Coach.new(name: "Юлия", filter: "сила, растяжка, фитбокс, функционалка, мфр")
-photo = File.open('app/assets/images/yulia_photo.png')
-coach2.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
-coach2.save!
+# coach2 = Coach.new(name: "Юлия", filter: "сила, растяжка, фитбокс, функционалка, мфр")
+# photo = File.open('app/assets/images/yulia_photo.png')
+# coach2.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
+# coach2.save!
 
-coach3 = Coach.new(name: "Яна", filter: "сила, функционалка, растяжка, после родов, мфр, лечебная")
-photo = File.open('app/assets/images/yana_photo.png')
-coach3.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
-coach3.save!
+# coach3 = Coach.new(name: "Яна", filter: "сила, функционалка, растяжка, после родов, мфр, лечебная")
+# photo = File.open('app/assets/images/yana_photo.png')
+# coach3.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
+# coach3.save!
 
-coach4 = Coach.new(name: "Амира", filter: "мфр, лечебная")
-photo = File.open('app/assets/images/amira_photo.png')
-coach4.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
-coach4.save!
+# coach4 = Coach.new(name: "Амира", filter: "мфр, лечебная")
+# photo = File.open('app/assets/images/amira_photo.png')
+# coach4.photo.attach(io: photo, filename: "golub_photo.png", content_type: "image/png")
+# coach4.save!
 
 # coach5 = Coach.new(name: "Темирлан", filter: "сила, функционалка")
 # # photo = URI.open("https://res.cloudinary.com/dmn9i4cwm/image/upload/v1678801134/cld-sample.jpg")
