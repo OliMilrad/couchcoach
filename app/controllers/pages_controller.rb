09dd9b23f032d513615e466ab7609e8355f4222c
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-09-18 19:01:06 +0300

    some mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+
   def home
+
     @head_coach = Coach.find_by(name: "Алёна")
     @coaches = Coach.all.without(@head_coach)
-   
+  
   end
 end
