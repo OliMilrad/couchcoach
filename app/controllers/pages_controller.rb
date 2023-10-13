541fe7f0e7fa910f15122d89c912843f0765783a
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-10-13 19:32:07 +0300

    docs routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :personal, :oferta, :agreement ]
 
 
   def home
 
     @head_coach = Coach.find_by(name: "Алёна")
     @coaches = Coach.all.without(@head_coach)
-  
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :coaches
   devise_for :users
+  get "/oferta", to: "pages#oferta"
+  get "/personal", to: "pages#personal"
+  get "/agreement", to: "pages#agreement"
+
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
