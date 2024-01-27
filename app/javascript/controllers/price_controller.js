d4499fde7d2b2571300ce1f8fb6c72a7cd848fb6
OliMilrad <manaenkova.olga1@gmail.com>
2024-01-27 13:40:14 +0200

    robokassa

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 const chatURL = "https://t.me/couch_coach_bot?start=";
+const payment4T_5225 ="https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&Description=4%20%D1%82%D1%80%D0%B5%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B8%20%D0%BF%D0%BE%2030%20%D0%BC%D0%B8%D0%BD%D1%83%D1%82&OutSum=5225&shp_interface=link&SignatureValue=6f8633d03009f8362fdc15cf5659522b"
+const payment4T_7315 = "https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&OutSum=7315&shp_interface=link&SignatureValue=88314fa696014d802f4a4cffa4bb867a"
+const payment8T_7007 = "https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&OutSum=7007&shp_interface=link&SignatureValue=0f632544533a9ceaffa2c09846d98d97"
+const payment8T_10465 = "https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&OutSum=10465&shp_interface=link&SignatureValue=4709019153efa1d1c785e576fcad723e"
+const payment24T_17000 = "https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&OutSum=17000&shp_interface=link&SignatureValue=196e09ecde9811c68c0ebf2dfd20326f"
+const payment24T_23545 = "https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=Couch_coach&InvId=0&Culture=ru&Encoding=utf-8&OutSum=23545&shp_interface=link&SignatureValue=492a850d66b6d8a30812c5525c5e47f0"
 
 export default class extends Controller {
   static targets = ["four_card_price_to_change", "four_card_half_time_to_change", "four_card_full_time_to_change", "eight_card_price_to_change", "eight_card_half_time_to_change", "eight_card_full_time_to_change", "twfour_card_price_to_change", "twfour_card_half_time_to_change", "twfour_card_full_time_to_change"]
@@ -19,13 +25,25 @@ export default class extends Controller {
 
     switch (numberOfTrainings) {
       case "4":
-        location.assign(chatURL + this.chosen_price_four);
+        if (this.chosen_price_four == 5225) {
+          location.assign(payment4T_5225);
+        } else {
+          location.assign(payment4T_7315);
+        }
         break;
       case "8":
-        location.assign(chatURL + this.chosen_price_eight);
+        if (this.chosen_price_eight == 7007) {
+          location.assign(payment8T_7007);
+        } else {
+          location.assign(payment8T_10465);
+        }
         break;
       case "24":
-        location.assign(chatURL + this.chosen_price_twfour);
+        if (this.chosen_price_twfour == 17000) {
+          location.assign(payment24T_17000);
+        } else {
+          location.assign(payment24T_23545);
+        }
         break;
       default:
         location.assign(chatURL + "0");
